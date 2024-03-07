01eafc3b7d7568df0b487230d504c94a1ca38903
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-07 17:42:42 +0530

    ADDED - 'MicrosoftEdge', '14.14393', 'Windows 10'

--- src/protractor.saucelabs.config.js
+++ src/protractor.saucelabs.config.js
@@ -34,6 +34,19 @@ exports.config = {
       },
     },
 
+    {
+      sauceUser: "oauth-pmsethulakshmi1994-71b99",
+      sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+      sauceBuild: "selenium-build-83FYR",
+      browserName: "MicrosoftEdge",
+      browserVersion: "14.14393",
+      platformName: "Windows 10",
+      "sauce:options": {
+        build: "selenium-build-83FYR",
+        name: "Sethu Test",
+        hostname: "ondemand.eu-central-1.saucelabs.com",
+      },
+    },
   ],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 60000,
