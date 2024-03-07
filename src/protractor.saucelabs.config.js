d352e6681fb9759618b583648338048ae2472c79
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-07 17:34:48 +0530

    ADDED - 'internet explorer', '11.103', 'Windows 10'

--- src/protractor.saucelabs.config.js
+++ src/protractor.saucelabs.config.js
@@ -15,10 +15,25 @@ exports.config = {
       platformName: "Windows 11",
       "sauce:options": {
         build: "selenium-build-83FYR",
-        name: "sethutestname",
+        name: "Sethu - chrome latest Windows 11",
         hostname: "ondemand.eu-central-1.saucelabs.com",
       },
     },
+
+    {
+      sauceUser: "oauth-pmsethulakshmi1994-71b99",
+      sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+      sauceBuild: "selenium-build-83FYR",
+      browserName: "internet explorer",
+      browserVersion: "11.103",
+      platformName: "Windows 10",
+      "sauce:options": {
+        build: "selenium-build-83FYR",
+        name: "Sethu Test",
+        hostname: "ondemand.eu-central-1.saucelabs.com",
+      },
+    },
+
   ],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 60000,
