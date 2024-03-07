2e2930d584cc496094d742aa758e0624b77121fd
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-07 17:49:19 +0530

    ADDED - 'MicrosoftEdge', '14.14393', 'Windows 10'

--- src/protractor.saucelabs.config.js
+++ src/protractor.saucelabs.config.js
@@ -6,20 +6,22 @@ exports.config = {
   sauceBuild: "selenium-build-83FYR",
   sauceRegion: "eu",
   multiCapabilities: [
-    {
-      sauceUser: "oauth-pmsethulakshmi1994-71b99",
-      sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
-      sauceBuild: "selenium-build-83FYR",
-      browserName: "chrome",
-      browserVersion: "latest",
-      platformName: "Windows 11",
-      "sauce:options": {
-        build: "selenium-build-83FYR",
-        name: "Sethu - chrome latest Windows 11",
-        hostname: "ondemand.eu-central-1.saucelabs.com",
-      },
-    },
+    // PASSED
+    // {
+    //   sauceUser: "oauth-pmsethulakshmi1994-71b99",
+    //   sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+    //   sauceBuild: "selenium-build-83FYR",
+    //   browserName: "chrome",
+    //   browserVersion: "latest",
+    //   platformName: "Windows 11",
+    //   "sauce:options": {
+    //     build: "selenium-build-83FYR",
+    //     name: "Sethu - chrome latest Windows 11",
+    //     hostname: "ondemand.eu-central-1.saucelabs.com",
+    //   },
+    // },
 
+    // FAILED
     // {
     //   sauceUser: "oauth-pmsethulakshmi1994-71b99",
     //   sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
@@ -34,19 +36,19 @@ exports.config = {
     //   },
     // },
 
-    // {
-    //   sauceUser: "oauth-pmsethulakshmi1994-71b99",
-    //   sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
-    //   sauceBuild: "selenium-build-83FYR",
-    //   browserName: "MicrosoftEdge",
-    //   browserVersion: "14.14393",
-    //   platformName: "Windows 10",
-    //   "sauce:options": {
-    //     build: "selenium-build-83FYR",
-    //     name: "Sethu Test",
-    //     hostname: "ondemand.eu-central-1.saucelabs.com",
-    //   },
-    // },
+    {
+      sauceUser: "oauth-pmsethulakshmi1994-71b99",
+      sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+      sauceBuild: "selenium-build-83FYR",
+      browserName: "MicrosoftEdge",
+      browserVersion: "14.14393",
+      platformName: "Windows 10",
+      "sauce:options": {
+        build: "selenium-build-83FYR",
+        name: "Sethu Test",
+        hostname: "ondemand.eu-central-1.saucelabs.com",
+      },
+    },
   ],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 60000,
