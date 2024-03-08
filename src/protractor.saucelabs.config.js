0b6ae1eb6a263863d3d1e5a1fd0c5bbdaf67cc48
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-08 11:14:15 +0530

    'safari', '9.0', 'OS X 10.11'

--- src/protractor.saucelabs.config.js
+++ src/protractor.saucelabs.config.js
@@ -36,16 +36,30 @@ exports.config = {
     //   },
     // },
 
+    // {
+    //   sauceUser: "oauth-pmsethulakshmi1994-71b99",
+    //   sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+    //   sauceBuild: "selenium-build-83FYR",
+    //   browserName: "MicrosoftEdge",
+    //   browserVersion: "14.14393",
+    //   platformName: "Windows 10",
+    //   "sauce:options": {
+    //     build: "selenium-build-83FYR",
+    //     name: "Sethu Test",
+    //     hostname: "ondemand.eu-central-1.saucelabs.com",
+    //   },
+    // },
+
     {
       sauceUser: "oauth-pmsethulakshmi1994-71b99",
       sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
       sauceBuild: "selenium-build-83FYR",
-      browserName: "MicrosoftEdge",
-      browserVersion: "14.14393",
-      platformName: "Windows 10",
+      browserName: "safari",
+      browserVersion: "9.0",
+      platformName: "OS X 10.11",
       "sauce:options": {
         build: "selenium-build-83FYR",
-        name: "Sethu Test",
+        name: "Sethu Test OS",
         hostname: "ondemand.eu-central-1.saucelabs.com",
       },
     },
