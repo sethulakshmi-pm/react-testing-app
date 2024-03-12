b83ebf6fbb9a797fb85705a9632f98a09001033f
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-03-12 09:36:43 +0530

    chrome latest Windows 11

--- src/protractor.saucelabs.config.js
+++ src/protractor.saucelabs.config.js
@@ -7,19 +7,19 @@ exports.config = {
   sauceRegion: "eu",
   multiCapabilities: [
     // PASSED
-    // {
-    //   sauceUser: "oauth-pmsethulakshmi1994-71b99",
-    //   sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
-    //   sauceBuild: "selenium-build-83FYR",
-    //   browserName: "chrome",
-    //   browserVersion: "latest",
-    //   platformName: "Windows 11",
-    //   "sauce:options": {
-    //     build: "selenium-build-83FYR",
-    //     name: "Sethu - chrome latest Windows 11",
-    //     hostname: "ondemand.eu-central-1.saucelabs.com",
-    //   },
-    // },
+    {
+      sauceUser: "oauth-pmsethulakshmi1994-71b99",
+      sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+      sauceBuild: "selenium-build-83FYR",
+      browserName: "chrome",
+      browserVersion: "latest",
+      platformName: "Windows 11",
+      "sauce:options": {
+        build: "selenium-build-83FYR",
+        name: "Sethu - chrome latest Windows 11",
+        hostname: "ondemand.eu-central-1.saucelabs.com",
+      },
+    },
 
     // FAILED
     // {
@@ -50,19 +50,19 @@ exports.config = {
     //   },
     // },
 
-    {
-      sauceUser: "oauth-pmsethulakshmi1994-71b99",
-      sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
-      sauceBuild: "selenium-build-83FYR",
-      browserName: "safari",
-      browserVersion: "9.0",
-      platformName: "OS X 10.11",
-      "sauce:options": {
-        build: "selenium-build-83FYR",
-        name: "Sethu Test OS",
-        hostname: "ondemand.eu-central-1.saucelabs.com",
-      },
-    },
+    // {
+    //   sauceUser: "oauth-pmsethulakshmi1994-71b99",
+    //   sauceKey: "c886851b-044e-4c37-8e9a-62867cea27a5",
+    //   sauceBuild: "selenium-build-83FYR",
+    //   browserName: "safari",
+    //   browserVersion: "9.0",
+    //   platformName: "OS X 10.11",
+    //   "sauce:options": {
+    //     build: "selenium-build-83FYR",
+    //     name: "Sethu Test OS",
+    //     hostname: "ondemand.eu-central-1.saucelabs.com",
+    //   },
+    // },
   ],
   jasmineNodeOpts: {
     defaultTimeoutInterval: 60000,
