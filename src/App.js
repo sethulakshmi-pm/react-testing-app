f89bcaa4ad67935bd992fd44718090dd82a5643c
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-04-18 15:45:22 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ import WebVitalComponent from "./component/WebVitalComponent";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <WebVitalComponent />
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://web.dev/articles/vitals"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Web Vitals
-        </a>
-      </header>
+      <WebVitalComponent />
     </div>
   );
 }
