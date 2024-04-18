12e278a2577db130cd93a26ab25a9743ccae9c7d
SETHULAKSHMI-PM <sethulakshmi.pm@ibm.com>
2024-04-18 15:57:14 +0530

    chrome driver updated-123

--- src/protractor.saucelabs.config.js
+++ src/protractor.saucelabs.config.js
@@ -35,73 +35,73 @@ exports.config = {
       "latest",
       "Windows 11",
       '"chrome", "latest", "Windows 11", '
-    ),
-    newSaucelabsCapability(
-      "internet explorer",
-      "11.103",
-      "Windows 10",
-      '"internet explorer", "11.103", "Windows 10"'
-    ),
-    newSaucelabsCapability(
-      "MicrosoftEdge",
-      "14.14393",
-      "Windows 10",
-      '"MicrosoftEdge", "14.14393", "Windows 10"'
-    ),
-    newSaucelabsCapability(
-      "safari",
-      "9.0",
-      "OS X 10.11",
-      '"safari", "9.0", "OS X 10.11"'
-    ),
-    newSaucelabsCapability(
-      "safari",
-      "10.1",
-      "macOS 10.12",
-      '"safari", "10.1", "macOS 10.12"'
-    ),
-    newSaucelabsCapability(
-      "safari",
-      "11.0",
-      "macOS 10.12",
-      '"safari", "11.0", "macOS 10.12"'
-    ),
-    newSaucelabsCapability(
-      "safari",
-      "11.1",
-      "macOS 10.13",
-      '"safari", "11.1", "macOS 10.13"'
-    ),
-    newSaucelabsCapability(
-      "firefox",
-      "78.0",
-      "Windows 7",
-      '"firefox", "78.0", "Windows 7"'
-    ),
-    newSaucelabsCapability(
-      "firefox",
-      "58.0",
-      "Windows 11",
-      '"firefox", "58.0", "Windows 11"'
-    ),
-    newSaucelabsCapability(
-      "chrome",
-      "67.0",
-      "Windows 10",
-      '"chrome", "67.0", "Windows 10"'
-    ),
-    newSaucelabsCapability(
-      "chrome",
-      "54.0",
-      "OS X 10.11",
-      '"chrome", "54.0", "OS X 10.11"'
-    ),
-    newSaucelabsCapability(
-      "chrome",
-      "65.0",
-      "OS X 10.11",
-      '"chrome", "65.0", "OS X 10.11"'
-    ),
+    )
+    // newSaucelabsCapability(
+    //   "internet explorer",
+    //   "11.103",
+    //   "Windows 10",
+    //   '"internet explorer", "11.103", "Windows 10"'
+    // ),
+    // newSaucelabsCapability(
+    //   "MicrosoftEdge",
+    //   "14.14393",
+    //   "Windows 10",
+    //   '"MicrosoftEdge", "14.14393", "Windows 10"'
+    // ),
+    // newSaucelabsCapability(
+    //   "safari",
+    //   "9.0",
+    //   "OS X 10.11",
+    //   '"safari", "9.0", "OS X 10.11"'
+    // ),
+    // newSaucelabsCapability(
+    //   "safari",
+    //   "10.1",
+    //   "macOS 10.12",
+    //   '"safari", "10.1", "macOS 10.12"'
+    // ),
+    // newSaucelabsCapability(
+    //   "safari",
+    //   "11.0",
+    //   "macOS 10.12",
+    //   '"safari", "11.0", "macOS 10.12"'
+    // ),
+    // newSaucelabsCapability(
+    //   "safari",
+    //   "11.1",
+    //   "macOS 10.13",
+    //   '"safari", "11.1", "macOS 10.13"'
+    // ),
+    // newSaucelabsCapability(
+    //   "firefox",
+    //   "78.0",
+    //   "Windows 7",
+    //   '"firefox", "78.0", "Windows 7"'
+    // ),
+    // newSaucelabsCapability(
+    //   "firefox",
+    //   "58.0",
+    //   "Windows 11",
+    //   '"firefox", "58.0", "Windows 11"'
+    // ),
+    // newSaucelabsCapability(
+    //   "chrome",
+    //   "67.0",
+    //   "Windows 10",
+    //   '"chrome", "67.0", "Windows 10"'
+    // ),
+    // newSaucelabsCapability(
+    //   "chrome",
+    //   "54.0",
+    //   "OS X 10.11",
+    //   '"chrome", "54.0", "OS X 10.11"'
+    // ),
+    // newSaucelabsCapability(
+    //   "chrome",
+    //   "65.0",
+    //   "OS X 10.11",
+    //   '"chrome", "65.0", "OS X 10.11"'
+    // ),
   ],
 
   jasmineNodeOpts: {
